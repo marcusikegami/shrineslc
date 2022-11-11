d9917136f9ff8070345b62ccee0cf2e23d1d17f3
Marcus Ikegami <hikegami.6@gmail.com>
2022-11-10 17:34:59 -0700

    added construction note

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
   return (
     <main>
       <Header/>
-      <div className='flex justify-center h-auto mx-40 my-8 '>
+      <div className='flex flex-col items-center justify-center h-auto mx-40 my-8 '>
         <img alt='shrine logo by Katya Pogodaeva 2022' src={shrinelogo}/>
+        <p className='font-bold text-4xl text-gray-500'>SITE UNDER CONSTRUCTION</p>
       </div>
     </main>
   );
