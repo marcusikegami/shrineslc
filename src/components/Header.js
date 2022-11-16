7668eb36c51b39144a335b128911772d1d988fff
Marcus Ikegami <hikegami.6@gmail.com>
2022-11-15 21:19:08 -0700

    updated header spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-      <div className="mx-auto lg:max-w-screen-2xl px-4  sticky top-0 bg-white">
+      <div className="mx-auto pt-2 pb-4 lg:max-w-screen-2xl px-4 sticky top-0 bg-white">
         <div className="flex items-center justify-between py-2 sm:py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
