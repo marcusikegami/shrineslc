0a9af8bfc04f7aca04fe4da032aac583f055abb1
Marcus Ikegami <hikegami.6@gmail.com>
2022-11-18 18:09:04 -0700

    updated header styles

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
       <Header/>
         <div className="flex justify-center py-2 border-t border-b border-darkpurple mx-16 my-2">
           <a href="https://www.youtube.com/channel/UCCJCWKOR156k8oA1ewXuKHQ" target="__blank" className="text-base font-medium text-gray-500 hover:text-red-600">
-            <AiFillYoutube className='h-6 sm:h-12 w-6 sm:w-12 mx-2' />
+            <AiFillYoutube className='h-8 sm:h-12 w-8 sm:w-12 mx-2' />
           </a>
           <a href="https://www.instagram.com/shrineslc/" target="__blank" className="text-base font-medium text-gray-500 hover:text-magenta">
-            <AiFillInstagram className='h-6 sm:h-12 w-6 sm:w-12 mx-2' />
+            <AiFillInstagram className='h-8 sm:h-12 w-8 sm:w-12 mx-2' />
           </a>
         </div>
       <body className='sm:mx-4 flex flex-col items-center'>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,20 @@
 
 export default function Header() {
   return (
-      <div className="mx-auto pt-2 pb-4 lg:max-w-screen-2xl px-4 sticky top-0 bg-white">
-        <div className="flex items-center justify-between py-2 sm:py-4 md:justify-start md:space-x-10">
+      <div className="mx-auto pt-2 lg:max-w-screen-2xl px-4 sticky top-0 bg-white">
+        <div className="flex items-center justify-between pt-2 pb-4 sm:py-4 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <a href="/">
               <span className="text-lg md:text-xl lg:text-3xl font-bold text-darkpurple border-b-4 border-darkpurple">SHRINE SLC</span>
             </a>
           </div>
           <div id="nav" className="-mr-2 -my-2">
-            <a href="#contact" className="text-xs sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-xl hover:text-darkpurple hover:underline underline-offset-4 px-2">Contact</a>
-            <a href="#video" className="text-xs sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-lg hover:text-darkpurple hover:underline underline-offset-4 px-2">Video</a>
-            <a href="#contact" className="text-xs sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-lg hover:text-darkpurple hover:underline underline-offset-4 px-2">Contact</a>
+            <a href="#contact" className="sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-xl hover:text-darkpurple hover:underline underline-offset-4 px-2">Contact</a>
+            <a href="#video" className="sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-lg hover:text-darkpurple hover:underline underline-offset-4 px-2">Video</a>
+            <a href="#contact" className="sm:text-l lg:text-xl drop-shadow-lg font-bold text-softblack hover:drop-shadow-lg hover:text-darkpurple hover:underline underline-offset-4 px-2">Contact</a>
           </div>
         </div>
+        <div className="w-full h-2 bg-gradient-to-b from-darkpurple" />
       </div>
   )
 };
